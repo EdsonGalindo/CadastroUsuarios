b3d23f64ddf9494f5f9a3ca922ede8bb068cde14
Edson Galindo <edcg@hotmail.com.br>
2018-09-19 03:02:15 -0300

    Criação do cadastro de usuários.

---
+++ CadastroUsuarios/CadastroUsuarios/Dao/UsuarioContext.cs
@@ -0,0 +1,16 @@
+﻿using CadastroUsuarios.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CadastroUsuarios.Dao
+{
+    public class UsuarioContext : DbContext
+    {
+        public UsuarioContext(DbContextOptions<UsuarioContext> options)
+            : base(options)
+        {
+
+        }
+
+        public DbSet<Usuario> Usuarios { get; set; }
+    }
+}
---
+++ CadastroUsuarios/CadastroUsuarios/Models/Usuario.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CadastroUsuarios.Models
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "O nome do usuário é requirido")]
+        public string Nome { get; set; }
+        [DisplayName("Data Nascimento")]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
+        [Required(ErrorMessage = "A data de nascimento do usuário é requirida")]
+        public DateTime DataNascimento { get; set; }
+        public bool Ativo { get; set; }
+    }
+}
